6266be904f7accbdb0c17c4cb6947f44c68b3cbb
Pretty-Flacko <48690875+Pretty-Flacko@users.noreply.github.com>
2025-12-06 16:23:01 +0400

    Add ability to create new projects

--- src/index.js
+++ src/index.js
@@ -5,13 +5,23 @@ import UI from "./ui.js";
 
 const projectsContainer = document.getElementById("projects-container");
 const todosContainer = document.getElementById("todos-container");
+const newProjectBtn = document.getElementById("new-project-btn");
 const ui = new UI(projectsContainer, todosContainer);
 
+const projects = [];
 const defaultProject = new Project("Default");
+projects.push(defaultProject);
 
 const todo1 = new Todo("Buy groceries", "Milk, eggs, bread", "2025-12-10", "medium");
 defaultProject.addTodo(todo1);
 
-const projects = [defaultProject];
-
 ui.renderProjects(projects);
+
+newProjectBtn.addEventListener("click", () => {
+    const name = prompt("Enter project name:", "New Project");
+    if (name && name.trim() !== "") {
+        const newProject = new Project(name.trim());
+        projects.push(newProject);
+        ui.renderProjects(projects);
+    }
+});
